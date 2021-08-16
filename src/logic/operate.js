5b66fc6a4863ac027ec3f3e1501b66f76d2af5af
bettercallazamat <bettercallazamat@gmail.com>
2021-08-16 22:56:24 +0600

    Add calculate and operate modules

---
+++ src/logic/operate.js
@@ -0,0 +1,50 @@
+import Big from 'big.js';
+
+const operate = (firstNum, secondNum, operation) => {
+  const numOne = new Big(firstNum);
+  const numTwo = new Big(secondNum);
+  let result;
+
+  switch (operation) {
+    case '+':
+      result = numOne.plus(numTwo);
+      break;
+
+    case '-':
+      result = numOne.minus(numTwo);
+      break;
+
+    case 'x':
+      result = numOne.times(numTwo);
+      break;
+
+    case '%':
+      result = numOne.mod(numTwo);
+      break;
+
+    case '/':
+      result = numOne.div(numTwo);
+      break;
+
+    case '+/-':
+      result = secondNumber
+        ? numTwo * -1
+        : numOne * -1;
+      break;
+
+    case 'AC':
+      result = 0;
+      break;
+
+    case '.':
+      result = 0.0;
+      break;
+
+    default:
+      break;
+  }
+
+  return result;
+};
+
+export default operate;
